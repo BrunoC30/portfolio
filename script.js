ae55ddfeceee142f1ad419cbdd1b52ef2a00969d
BrunoC30 <bruno01eufonio@gmail.com>
2025-10-20 16:13:53 -0300

    contatos

--- script.js
+++ script.js
@@ -9,20 +9,52 @@ const layer2 = document.querySelector("#layer2");
 const btaoExit = document.querySelector("#exit");
 const cardImg = document.querySelector(".image")
 const cardTitulo = document.querySelector(".textos h1");
-const descricao = document.querySelector("metaDados");
+const cardDescricao = document.querySelector(".textos p");
+const linkbtao = document.querySelector("#linkBtao");
 
 //lista de projetos
 const projetos = document.querySelectorAll(".cardP");
+const verMais = document.querySelector("#verMais");
+let ativo = false;
 
-//todos os projetos tem lógica de card
+//todos os projetos tem lógica de abrir card
 projetos.forEach(card=>{
     card.addEventListener("click",()=>{
         //abrir card
         layer2.style.display="flex";
         cardImg.style.backgroundImage = window.getComputedStyle(card).backgroundImage;
-        cardTitulo.textContent= card.getAttribute("name");
+
+        if(card.getAttribute("name")===""){
+            cardTitulo.textContent="Sem título"
+        }else{
+            cardTitulo.textContent = card.getAttribute("name");
+            linkbtao.href= card.dataset.link;
+        }
+        if(card.dataset.d===""){
+            cardDescricao.textContent="em breve"
+        }else{
+
+            cardDescricao.textContent = card.dataset.d;
+        }
 
     })
+})
+//aumentar e diminuir o tamanho no mobile
+verMais.addEventListener("click",()=>{
+    const containerProjetos = document.getElementById("containerProjetos");
+    const cards = document.querySelector(".cards")
+
+    if(ativo===false){
+        ativo=true;
+        containerProjetos.style.height="500px";
+        cards.style.gridTemplateRows="2fr 2fr 2fr";
+        verMais.textContent="ver menos";
+    }else{
+        ativo=false;
+        containerProjetos.style.height="260px";
+        cards.style.gridTemplateRows="100%";
+        verMais.textContent="ver mais";
+    }
 })
 
 //lógica de fechar card
